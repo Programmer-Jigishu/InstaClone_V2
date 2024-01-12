11f57bce206fd4363e3682b8d5e13761f1e41921
Jigishu B <jigishuatprogramming@gmail.com>
2024-01-12 16:48:17 +0530

    Commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes,Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import AuthPage from "./pages/AuthPage/AuthPage";
-import HomePageLayout from "./layouts/homePageLayout";
+import PageLayout from "./layouts/HomePageLayout";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 // 1
 import useAuthStore from "./store/authStore";
@@ -13,13 +13,13 @@ function App() {
   // const authUser = useAuthStore(state => state.user);
   const authUser = useAuthState(auth) && useAuthStore(state => state.user);
   return (
-    <HomePageLayout>
+    <PageLayout>
       <Routes>
         <Route path="/" element={authUser?<HomePage/>:<Navigate to="/auth"/>}/>
         <Route path="/auth" element={authUser?<Navigate to="/"/>:<AuthPage/>}/>
         <Route path="/:username" element={<ProfilePage/>} />
       </Routes>
-    </HomePageLayout>
+    </PageLayout>
   );
 }
 
